8262a8656c2891b09e89b51b17985e7c4080c70b
abelokur <abelokur@yandex.ru>
2020-08-23 22:49:44 +0300

    5.6. Простые числа [#345104]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,11 +3,14 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = false;
-        for (int index = 2; index < number - 1; index++) {
+        for (int index = 2; index < number; index++) {
             if (number % index == 0) {
                 prime = false;
                 break;
             }
+            prime = true;
+        }
+        if (number == 2) {
             prime = true;
         }
         return prime;
