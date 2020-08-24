1c0b06b502bf12d24143d0a4a37d7d7e8f1a9da1
abelokur <abelokur@yandex.ru>
2020-08-24 23:44:42 +0300

    5.6. Простые числа [#345104] Исправил Объявил boolean prime = number > 1

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,15 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
+        boolean prime = number > 1;
         for (int index = 2; index < number; index++) {
             if (number % index == 0) {
                 prime = false;
                 break;
             }
-            prime = true;
-        }
-        if (number == 2) {
             prime = true;
         }
         return prime;
