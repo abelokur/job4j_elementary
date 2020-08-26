4a8ba63168b9551b6ff05534d11580c046eee831
abelokur <abelokur@yandex.ru>
2020-08-26 21:46:13 +0300

    6.0.2. Заполнение массива. [#345030]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,18 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surname равен: " + surname.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String names[] = new String[4];
+
+        names[0] = "Andrey Belokur";
+        names[1] = "Andrey Semenov";
+        names[2] = "Sergei Kovalenko";
+        names[3] = "Aleksandr Stepanov";
+
+            System.out.println(names[0]);
+            System.out.println(names[1]);
+            System.out.println(names[2]);
+            System.out.println(names[3]);
+
     }
 }
