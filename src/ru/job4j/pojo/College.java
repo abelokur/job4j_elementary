4a23fa6e7efddbb2e74f0f48167714d8a47a39bc
abelokur <abelokur@yandex.ru>
2020-09-09 22:37:49 +0300

    Исправил College вызывался метод getFIO изменил на getFio

--- src/ru/job4j/pojo/College.java
+++ src/ru/job4j/pojo/College.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class College {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setFIO("Andrey Belokur");
+        student.setFio("Andrey Belokur");
         student.setGroup("2195");
         student.setEnterdate(new Date());
 
-        System.out.println(student.getFIO() + " is a student of " + student.getGroup() + "'s group since " + student.getEnterdate());
+        System.out.println(student.getFio() + " is a student of " + student.getGroup() + "'s group since " + student.getEnterdate());
     }
 }
