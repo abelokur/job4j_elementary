7f34f5f50f568c7176113d3ae80e4f134c921a0d
abelokur <abelokur@yandex.ru>
2020-09-09 20:46:20 +0300

    3. Массивы и модели. [#345229] исправил на equals

--- src/ru/job4j/pojo/Library.java
+++ src/ru/job4j/pojo/Library.java
@@ -24,7 +24,7 @@ public class Library {
         }
         for (int index = 0; index < books.length; index++) {
             Book bk = books[index];
-            if (bk.getName() == "Clean code") {
+            if (bk.getName().equals("Clean code")) {
                 System.out.println(bk.getName() + " " + bk.getPages());
             }
         }
